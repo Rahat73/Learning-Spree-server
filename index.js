65be5f0a3dfd77de9d1af5093a9ccbabe4b6a721
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-26 01:29:02 +0600

    courses api added

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Learning-Spree API running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+
 
 app.listen(port, () => {
     console.log('Learning-Spree API running on port', port);
